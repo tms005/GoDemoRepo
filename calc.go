2732fa3a687329397044be625aebb0f713f4f315
lkh22 <lkh22@np.edu.sg>
2021-05-27 16:18:30 +0800

    this is initial commit of comments

--- calc.go
+++ calc.go
@@ -3,7 +3,7 @@ package calc
 // Add two numbers and return the result.
 func Add(val1, val2 int) int {
 	return val1 + val2
-}
+}//
 
 // Subtract two numbers and return the result.
 func Subtract(val1, val2 int) int {
